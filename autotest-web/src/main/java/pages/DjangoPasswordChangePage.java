7b49b7c72a67a1406c145f8baf2e00e1e45a9477
Valeria Pohilenko <valeriapohilenko@gmail.com>
2021-11-12 00:26:02 +0300

    add DjangoPasswordChangePage.java

---
+++ autotest-web/src/main/java/pages/DjangoPasswordChangePage.java
@@ -0,0 +1,20 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import ru.lanit.at.web.annotations.Name;
+import ru.lanit.at.web.pagecontext.WebPage;
+
+import static com.codeborne.selenide.Selenide.$;
+
+@Name(value = "DjangoPasswordChange")
+public class DjangoPasswordChangePage extends WebPage {
+    @Name("старый пароль")
+    private SelenideElement oldPassword = $(By.id("id_old_password"));
+    @Name("новый пароль")
+    private SelenideElement newPassword = $(By.id("id_new_password1"));
+    @Name("подтверждение нового пароля")
+    private SelenideElement newPasswordConfirmation = $(By.id("id_new_password2"));
+    @Name("изменить пароль")
+    private SelenideElement changePassword = $(By.id("//input[@type='submit']"));
+}
