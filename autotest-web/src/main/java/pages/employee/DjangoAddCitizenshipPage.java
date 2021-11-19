42b98027f879f57e1c273f3efee6fdeda2b36e2a
Valeria Pohilenko <valeriapohilenko@gmail.com>
2021-11-19 10:55:45 +0300

    добавлены тк 7.3-7.9, добавлены элементы в DjangoEmployeeChangePage, создан новый класс DjangoAddCitizenshipPage

---
+++ autotest-web/src/main/java/pages/employee/DjangoAddCitizenshipPage.java
@@ -0,0 +1,20 @@
+package pages.employee;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.lanit.at.web.annotations.Name;
+import ru.lanit.at.web.pagecontext.WebPage;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+@Name(value = "DjangoAddCitizenship")
+public class DjangoAddCitizenshipPage extends WebPage {
+    @Name("Гражданство")
+    private SelenideElement citizenship = $x("//input[@id='id_name']");
+    @Name("Сохранить")
+    private SelenideElement save = $x("//input[@type='submit']");
+    @Name("Добавить Гражданство")
+    private SelenideElement addCitizenship = $("#content-main");
+
+
+}
