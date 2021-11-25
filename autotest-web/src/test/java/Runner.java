611253f4f0df5417bb1e29efe3f1d7418b6706d1
Vitaly Zverev <93658720+VitalyZverev@users.noreply.github.com>
2021-11-25 17:21:36 +0300

    Восстановлен класс Runner (#30)
    * Restored Runner
    
    * Update Runner.java
    
    Co-authored-by: Vitaly Zverev <vzhka@post.cz>

---
+++ autotest-web/src/test/java/Runner.java
@@ -0,0 +1,20 @@
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"
+        },
+        features = "classpath:features",
+        glue = {"steps", "hooks"}
+)
+public class Runner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
